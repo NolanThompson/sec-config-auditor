d859860a428a9aeb1261f97bbd2bb564c572e2e6
manielsen13 <marshall.niels@gmail.com>
2023-12-04 20:21:10 -0500

    Add another flag for cloud provider. Add checks

--- run_audit.sh
+++ run_audit.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Check if flag is passed
-if [ $# -eq 0 ]; then
-    echo "No arguments provided. Please provide a flag for either 'summary' or 'recommendations'."
+# Check if the correct number of arguments is passed
+if [ $# -ne 2 ]; then
+    echo "Invalid number of arguments. Usage: $0 [cloud_provider] [audit_type]"
     exit 1
 fi
 
-FLAG=$1
+CLOUD_PROVIDER=$1
+AUDIT_TYPE=$2
 CONFIG_FILE="prompt-config.json"
+
+# Validate the cloud provider
+if [ "$CLOUD_PROVIDER" != "aws" ] && [ "$CLOUD_PROVIDER" != "azure" ]; then
+    echo "Invalid cloud provider specified. Please specify either 'aws' or 'azure'."
+    exit 1
+fi
 
 # Check if config file exists
 if [ ! -f $CONFIG_FILE ]; then
@@ -15,14 +22,14 @@ if [ ! -f $CONFIG_FILE ]; then
     exit 1
 fi
 
-# Read the prompt based on the flag
-PROMPT=$(jq -r ".$FLAG // empty" $CONFIG_FILE)
+# Read the prompt based on the audit type
+PROMPT=$(jq -r ".${AUDIT_TYPE} // empty" $CONFIG_FILE)
 
 # Check if prompt is empty or null
 if [ -z "$PROMPT" ] || [ "$PROMPT" == "null" ]; then
-    echo "Invalid flag or no prompt defined for this flag."
+    echo "Invalid audit type or no prompt defined for this audit type."
     exit 1
 fi
 
-# Run the Python script with the prompt
-python src/main.py "$PROMPT"
+# Run the Python script with the cloud provider and the prompt
+python src/main.py "$CLOUD_PROVIDER" "$PROMPT"
